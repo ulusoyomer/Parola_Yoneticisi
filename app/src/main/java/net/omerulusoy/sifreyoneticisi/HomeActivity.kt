e0642726c0452bcd897b23a2c0f4d2b091c57a63
Ömer Ulusoy <ulusoy.omer09@gmail.com>
2021-04-27 01:54:03 +0300

    Create Password Done

---
+++ app/src/main/java/net/omerulusoy/sifreyoneticisi/HomeActivity.kt
@@ -0,0 +1,70 @@
+package net.omerulusoy.sifreyoneticisi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    private val alphabet = arrayOf(
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+        "P", "R", "S", "T", "U", "V", "Y", "Z", "X", "W"
+    )
+    private val trChars = arrayOf("Ç", "Ğ", "İ", "Ö", "Ş", "Ü", "ı")
+    private val symbols = arrayOf(
+        "~",
+        "`",
+        "!",
+        "@",
+        "#",
+        "£",
+        "€",
+        "$",
+        "¢",
+        "¥",
+        "§",
+        "%",
+        "°",
+        "^",
+        "&",
+        "*",
+        "(",
+        ")",
+        "-",
+        "_",
+        "+",
+        "=",
+        "{",
+        "}",
+        "[",
+        "]",
+        "|",
+        "\\",
+        "/",
+        ":",
+        ";",
+        "\"",
+        "'",
+        "<",
+        ">",
+        ",",
+        ".",
+        "?"
+    )
+    private val numbers = arrayOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+    private val methods = mapOf<String, Array<String>>(
+        "alphabet" to alphabet, "trChars" to trChars,
+        "symbols" to symbols, "numbers" to numbers
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+    fun createPassword(length: Int, selectedMethods: Array<String>): String {
+        var tmpString = ""
+        for (i in 0..length) {
+            tmpString += this.methods[selectedMethods.random()].random()
+        }
+        return tmpString
+    }
+}
--- app/src/main/java/net/omerulusoy/sifreyoneticisi/MainActivity.kt
+++ app/src/main/java/net/omerulusoy/sifreyoneticisi/MainActivity.kt
@@ -2,19 +2,11 @@ package net.omerulusoy.sifreyoneticisi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import net.sqlcipher.database.SQLiteDatabase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-    }
-
-    fun createPassword(length:Int){
-        for (i in 0..length){
-
-        }
 
     }
 
