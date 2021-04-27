6f0fcfa5902cb949f57fc44f2cb41156e76c9387
Ömer Ulusoy <ulusoy.omer09@gmail.com>
2021-04-28 02:47:40 +0300

    First Page Db Controls Add

--- app/src/main/java/net/omerulusoy/sifreyoneticisi/MainActivity.kt
+++ app/src/main/java/net/omerulusoy/sifreyoneticisi/MainActivity.kt
@@ -1,13 +1,46 @@
 package net.omerulusoy.sifreyoneticisi
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+    private val dbNames  = ArrayList<String>()
+    private var dbFiles : Array<File>? = File("data/data/net.omerulusoy.sifreyoneticisi/databases").listFiles()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_ŞifreYöneticisi)
         setContentView(R.layout.activity_main)
+
+
+
     }
 
+    fun getAllDbNames(){
+        if (dbFiles != null) {
+           if (dbNames.isNotEmpty())
+               dbNames.clear()
+            for (dbFile in dbFiles as Array<out File>){
+                dbNames.add(dbFile.name)
+            }
+        }
+    }
+
+    fun printAllDbNames(){
+        if (dbNames.isNotEmpty()){
+            for (each in dbNames){
+                Log.d("Db Name", each)
+            }
+        }
+    }
+
+    fun clearDBFiles(){
+        if(dbFiles != null){
+            for (dbFile in dbFiles as Array<out File>){
+                dbFile.delete()
+            }
+        }
+    }
 }
