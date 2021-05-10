5cd6d926eadbda9d4c70f86dc578875f3d04ca97
Ömer Ulusoy <ulusoy.omer09@gmail.com>
2021-05-10 03:47:14 +0300

    Import Database Done !

--- app/src/main/java/net/omerulusoy/sifreyoneticisi/MainActivity.kt
+++ app/src/main/java/net/omerulusoy/sifreyoneticisi/MainActivity.kt
@@ -2,26 +2,34 @@ package net.omerulusoy.sifreyoneticisi
 
 
 import android.annotation.SuppressLint
+import android.content.ContentUris
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
+import android.os.Environment
+import android.provider.DocumentsContract
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import es.dmoral.toasty.Toasty
 import net.omerulusoy.sifreyoneticisi.SQLiteOperations.SQLiteConnector.SQLiteConnector
 import net.omerulusoy.sifreyoneticisi.SQLiteOperations.SQLiteCreator.SQLiteCreator
-import java.io.File
 import net.sqlcipher.database.SQLiteDatabase
+import java.io.File
+import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
     private val dbNames = ArrayList<String>()
-
+    private val dbImportCode: Int = 1
     private var dbFiles: Array<File>? = null
 
     private lateinit var adapterAllDbNameList: HomeListAdapter
@@ -29,6 +37,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var title: TextView
     private lateinit var btnNewDb: Button
     private lateinit var btnOpenDb: Button
+
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,29 +61,38 @@ class MainActivity : AppCompatActivity() {
         SQLiteDatabase.loadLibs(this)
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d("START", "START")
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && data != null) {
+            when (requestCode) {
+                dbImportCode -> {
+                    try {
+                        val dbFile = File(data.data?.let { getPathFromUri(this, it) })
+                        val newDBFile = getDatabasePath(dbFile.name)
+                        if (newDBFile.exists()) newDBFile.delete()
+                        newDBFile.mkdirs()
+                        newDBFile.delete()
+                        dbFile.copyTo(newDBFile)
+                        Toasty.success(this, "Veritabanı Aktarıldı", Toast.LENGTH_SHORT, true)
+                            .show()
+                    } catch (e: Exception) {
+                        Toasty.error(this, "Veritabanı Aktarılamadı", Toast.LENGTH_SHORT, true)
+                            .show()
+                    }finally {
+                        updateDBList()
+                    }
+                }
+            }
+        }
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.d("onResume", "onResume")
-    }
 
-    override fun onRestart() {
-        super.onRestart()
-        Log.d("onRestart", "onRestart")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d("onPause", "onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d("onStop", "onStop")
+    fun showChooser(view: View) {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true)
+        startActivityForResult(intent, dbImportCode)
     }
 
     fun getAllDbNames() {
@@ -84,7 +102,6 @@ class MainActivity : AppCompatActivity() {
                 dbNames.clear()
             for (dbFile in dbFiles as Array<out File>) {
                 dbNames.add(dbFile.name)
-                Log.d("DBNAME", dbFile.name)
             }
         }
     }
@@ -104,6 +121,13 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun updateDBList(){
+        getAllDbNames()
+        adapterAllDbNameList.notifyDataSetChanged()
+        printAllDbNames()
+    }
+
+
     fun openNewDbScreen(view: View) {
         view as Button
         val newDBLayout = LayoutInflater.from(this).inflate(R.layout.home_open_new_db, null)
@@ -121,10 +145,7 @@ class MainActivity : AppCompatActivity() {
                     if (!dbNames.contains(dbName.text.toString())) {
                         val db =
                             SQLiteCreator(this, dbName.text.toString(), password.text.toString())
-                        getAllDbNames()
-                        adapterAllDbNameList.notifyDataSetChanged()
-                        File("data/data/net.omerulusoy.sifreyoneticisi/databases").listFiles()
-                        printAllDbNames()
+                            updateDBList()
                         Toasty.success(
                             this,
                             "Veritabanı Başarıyla Oluşturuldu !",
@@ -142,6 +163,118 @@ class MainActivity : AppCompatActivity() {
         alertDialog.show()
     }
 
+    fun getPathFromUri(context: Context, uri: Uri): String? {
+        val isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":").toTypedArray()
+                val type = split[0]
+                if ("primary".equals(type, ignoreCase = true)) {
+                    return Environment.getExternalStorageDirectory().toString() + "/" + split[1]
+                }
+
+                // TODO handle non-primary volumes
+            } else if (isDownloadsDocument(uri)) {
+                val id = DocumentsContract.getDocumentId(uri)
+                val contentUri: Uri = ContentUris.withAppendedId(
+                    Uri.parse("content://downloads/public_downloads"), java.lang.Long.valueOf(id)
+                )
+                return getDataColumn(context, contentUri, null, null)
+            } else if (isMediaDocument(uri)) {
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":").toTypedArray()
+                val type = split[0]
+                var contentUri: Uri? = null
+                if ("image" == type) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                } else if ("video" == type) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+                } else if ("audio" == type) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+                }
+                val selection = "_id=?"
+                val selectionArgs = arrayOf(
+                    split[1]
+                )
+                return getDataColumn(context, contentUri, selection, selectionArgs)
+            }
+        } else if ("content".equals(uri.getScheme(), ignoreCase = true)) {
+
+            // Return the remote address
+            return if (isGooglePhotosUri(uri)) uri.getLastPathSegment() else getDataColumn(
+                context,
+                uri,
+                null,
+                null
+            )
+        } else if ("file".equals(uri.getScheme(), ignoreCase = true)) {
+            return uri.getPath()
+        }
+        return null
+    }
+
+    fun getDataColumn(
+        context: Context, uri: Uri?, selection: String?,
+        selectionArgs: Array<String>?
+    ): String? {
+        var cursor: Cursor? = null
+        val column = "_data"
+        val projection = arrayOf(
+            column
+        )
+        try {
+            cursor = uri?.let {
+                context.contentResolver.query(
+                    it, projection, selection, selectionArgs,
+                    null
+                )
+            }
+            if (cursor != null && cursor.moveToFirst()) {
+                val index: Int = cursor.getColumnIndexOrThrow(column)
+                return cursor.getString(index)
+            }
+        } finally {
+            if (cursor != null) cursor.close()
+        }
+        return null
+    }
+
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    fun isExternalStorageDocument(uri: Uri): Boolean {
+        return "com.android.externalstorage.documents" == uri.getAuthority()
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    fun isDownloadsDocument(uri: Uri): Boolean {
+        return "com.android.providers.downloads.documents" == uri.getAuthority()
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    fun isMediaDocument(uri: Uri): Boolean {
+        return "com.android.providers.media.documents" == uri.getAuthority()
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is Google Photos.
+     */
+    fun isGooglePhotosUri(uri: Uri): Boolean {
+        return "com.google.android.apps.photos.content" == uri.getAuthority()
+    }
 
     class HomeListAdapter(private val cntx: Context, private val names: ArrayList<String>) :
         ArrayAdapter<String>(cntx, R.layout.home_list_row, names) {
