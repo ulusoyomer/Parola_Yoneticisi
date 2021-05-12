f58e7ac13c17ed1dd1f21a4b5644aeb565e23c70
Ömer Ulusoy <ulusoy.omer09@gmail.com>
2021-05-12 21:58:24 +0300

    Bottom Navigation -2

---
+++ app/src/main/java/net/omerulusoy/sifreyoneticisi/AddAccountFragment.kt
@@ -0,0 +1,31 @@
+package net.omerulusoy.sifreyoneticisi
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+
+
+class AddAccountFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_add_account, container, false)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() =
+            AddAccountFragment().apply {
+                arguments = Bundle().apply {}
+            }
+    }
+}
--- app/src/main/java/net/omerulusoy/sifreyoneticisi/HomeActivity.kt
+++ app/src/main/java/net/omerulusoy/sifreyoneticisi/HomeActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation
 import es.dmoral.toasty.Toasty
 
@@ -60,7 +62,7 @@ class HomeActivity : AppCompatActivity() {
         "alphabet" to alphabet, "trChars" to trChars,
         "symbols" to symbols, "numbers" to numbers
     )
-
+    private var navAnim = 0
     lateinit var bottomNavigation: MeowBottomNavigation
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -73,25 +75,25 @@ class HomeActivity : AppCompatActivity() {
         )
         setContentView(R.layout.activity_home)
         bottomNavigation = findViewById(R.id.bottomNavigation)
-        bottomNavigation.add(MeowBottomNavigation.Model(0,R.drawable.ic_home))
-        bottomNavigation.add(MeowBottomNavigation.Model(1,R.drawable.ic_add))
-        bottomNavigation.add(MeowBottomNavigation.Model(2,R.drawable.ic_settings))
-
+        bottomNavigation.add(MeowBottomNavigation.Model(0, R.drawable.ic_home))
+        bottomNavigation.add(MeowBottomNavigation.Model(1, R.drawable.ic_add))
+        bottomNavigation.add(MeowBottomNavigation.Model(2, R.drawable.ic_settings))
+        addFragment(HomeFragment.newInstance())
         bottomNavigation.show(0)
 
         bottomNavigation.setOnClickMenuListener {
-            when(it.id){
+            when (it.id) {
                 0 -> {
-                    Toasty.success(this,"Home",Toast.LENGTH_SHORT).show()
+                    replaceFragment(HomeFragment.newInstance())
                 }
                 1 -> {
-                    Toasty.success(this,"Add",Toast.LENGTH_SHORT).show()
+                    replaceFragment(AddAccountFragment.newInstance())
                 }
                 2 -> {
-                    Toasty.success(this,"Settings",Toast.LENGTH_SHORT).show()
+                    replaceFragment(SettingsFragment.newInstance())
                 }
                 else -> {
-                    Toasty.success(this,"Nothing",Toast.LENGTH_SHORT).show()
+                    Toasty.success(this, "Nothing", Toast.LENGTH_SHORT).show()
                 }
             }
         }
@@ -99,24 +101,50 @@ class HomeActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
-        if (backTap)
-            super.onBackPressed()
-        else{
-            Toasty.info(
-                this,
-                "Çıkmak İçin Birdaha Basın",
-                Toast.LENGTH_SHORT,
-                true
-            )
-                .show()
-            backTap = true
-            Handler().postDelayed({
-                run{
-                    backTap = false
-                }
-            },2000)
+        if (navAnim <= 0){
+            if (backTap)
+                super.onBackPressed()
+            else {
+                Toasty.info(
+                    this,
+                    "Çıkmak İçin Birdaha Basın",
+                    Toast.LENGTH_SHORT,
+                    true
+                )
+                    .show()
+                backTap = true
+                Handler().postDelayed({
+                    run {
+                        backTap = false
+                    }
+                }, 2000)
 
+            }
         }
+        else{
+            navAnim--
+            super.onBackPressed()
+        }
+
+    }
+
+    fun replaceFragment(fragment: Fragment) {
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+        fragmentTransition.setCustomAnimations(
+            R.anim.animate_slide_up_enter,
+            R.anim.animate_slide_down_exit
+        )
+        fragmentTransition.replace(R.id.frame_container, fragment)
+            .addToBackStack(Fragment::class.java.simpleName).commit()
+        navAnim++
+    }
+
+    fun addFragment(fragment: Fragment) {
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+
+        fragmentTransition.add(R.id.frame_container, fragment)
+            .addToBackStack(Fragment::class.java.simpleName).commit()
+
     }
 
     fun createPassword(length: Int, selectedMethods: Array<String>): String {
